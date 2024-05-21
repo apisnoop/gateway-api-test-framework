132a6edc91ad62746a914cccdd38de1ef763e83f
Stephen Heywood <stephen@ii.coop>
2024-05-21 12:46:51 +1200

    feat: add conformance script with cilium update

---
+++ run-conformance-suite.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+echo "CONFIG_DIR: ${CONFIG_DIR}"
+export IMPLEMENTATION=${IMPLEMENTATION,,}
+echo "IMPLEMENTATION: ${IMPLEMENTATION}"
+
+if [[ "${IMPLEMENTATION}" == "none" ]] ; then
+  echo "Error: Deploy an implementation first."
+  exit 1
+fi
+
+export IMPLEMENTATION_REPO_PATH=${IMPLEMENTATION_REPO_PATH:-"${PWD}/repos/${IMPLEMENTATION}"}
+source "./lib/init.sh"
+
+run::${IMPLEMENTATION}::conformance
